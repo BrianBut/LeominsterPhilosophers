d836129b560b2766b315f0cd5f8f8168a2b8b28a
Brian Butcher <brian@debian.home>
2023-09-29 10:00:23 +0100

    modified:   app/email.py

--- app/email.py
+++ app/email.py
@@ -5,8 +5,8 @@ from . import mail
 
 
 def send_async_email(app, msg):
-    #with app.app_context():
-    #    mail.send(msg)
+    with app.app_context():
+        mail.send(msg)
     return
 
 
@@ -15,9 +15,9 @@ def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['LP_MAIL_SUBJECT_PREFIX'] + ' ' + subject, sender=app.config['LP_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    print("sender: {}".format(msg.sender))
-    print("recipients: {}".format(msg.recipients))
-    print("body: {}".format(msg.body))
+    #print("sender: {}".format(msg.sender))
+    #print("recipients: {}".format(msg.recipients))
+    #print("body: {}".format(msg.body))
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
